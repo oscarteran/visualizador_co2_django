cb646ea1e037536eb2ed9f0579b9c2b357f45436
oscarteran <oscar-hdzteran@hotmail.com>
2025-05-13 16:43:10 -0600

    Cambio de estilos correctos

--- Django/config/settings.py
+++ Django/config/settings.py
@@ -109,8 +109,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
+# Esto es necesario si usas archivos estáticos durante desarrollo
+STATICFILES_DIRS = [
+    BASE_DIR / "mapviewer" / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
