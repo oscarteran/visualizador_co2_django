02876b2c6ad98701995fec97995a821e3078e90d
oscarteran <oscar-hdzteran@hotmail.com>
2025-05-13 17:01:03 -0600

    Vista completa como streamlit

--- Django/mapviewer/urls.py
+++ Django/mapviewer/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    
+    #path('', views.catalogo, name='catalogo'),
+    path('inventario_nacional/', views.inventario_nacional, name='inventario_nacional'),
+    path('ficha_tec/', views.ficha_tec, name='ficha_tec'),
+    path('fuentes_info/', views.fuentes_info, name='fuentes_info')
 ]
--- Django/mapviewer/views.py
+++ Django/mapviewer/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'mapviewer/inicio.html')
+
+def catalogo(request):
+    return render(request, 'mapviewer/catalogo.html')
+
+def inventario_nacional(request):
+    return render(request, 'mapviewer/inventario_nacional.html')
+
+def ficha_tec(request):
+    return render(request, 'mapviewer/ficha_tecnica.html')
+
+def fuentes_info(request):
+    return render(request, 'mapviewer/fuentes_info.html')
