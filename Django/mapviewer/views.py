08f09cc61c3e066d1c9b7d34cebf1a3b4a154acc
oscarteran <oscar-hdzteran@hotmail.com>
2025-05-16 13:43:04 -0600

    Mapa base en ficha tecnica

--- Django/mapviewer/views.py
+++ Django/mapviewer/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-from .utils.mapas import generar_mapa_html
+from .utils.mapas import generar_mapa_html, generar_mapas_individuales
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,14 +11,6 @@ def inicio(request):
 
 def catalogo(request):
     return render(request, 'mapviewer/catalogo.html')
-
-# def inventario_nacional(request):
-#     mapa_html = generar_mapa_html('data/processed/grafico_nacional.json')
-#     if not mapa_html:
-#         return render(request, 'mapviewer/error.html', {'error': 'No se pudo generar el mapa.'})
-    
-#     # Pasamos el HTML del mapa como contexto
-#     return render(request, 'mapviewer/inventario_nacional.html', {'mapa': mapa_html})
 
 def inventario_nacional(request):
     # Obtener el tipo de mapa seleccionado del parámetro GET
@@ -45,7 +37,25 @@ def inventario_nacional(request):
     return render(request, 'mapviewer/inventario_nacional.html', context)
 
 def ficha_tec(request):
-    return render(request, 'mapviewer/ficha_tecnica.html')
+    # Obtener el tipo de mapa seleccionado del parámetro GET
+    tipo_mapa = request.GET.get('tipo_mapa', 'División Política')
+    
+    # Generar el mapa con el tipo seleccionado
+    mapa_html = generar_mapas_individuales('Alcaparrosa')
+    
+    if not mapa_html:
+        return render(request, 'mapviewer/error.html', {'error': 'No se pudo generar el mapa.'})
+    
+    # Obtener las opciones de mapa para el dropdown
+    opciones_mapa = ["División Política", "Satelital", "Relieve"]
+    
+    context = {
+        'mapa': mapa_html,
+        'tipo_mapa_seleccionado': 'tipo_mapa',
+        'opciones_mapa': opciones_mapa
+    }
+    
+    return render(request, 'mapviewer/ficha_tecnica.html', context)
 
 def fuentes_info(request):
     return render(request, 'mapviewer/fuentes_info.html')
