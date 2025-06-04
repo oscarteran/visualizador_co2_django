abef152cd05d5d1becbf673572579bc83994af42
oscarteran <oscar-hdzteran@hotmail.com>
2025-06-03 18:28:26 -0600

    Version en local

--- mapviewer/admin.py
+++ mapviewer/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Producto
+# from .models import Producto
 
-admin.site.register(Producto)
+# admin.site.register(Producto)
